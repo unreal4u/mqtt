c2ca97db92878f2fc16b6fbd41b514e688358586
Camilo Sperberg <me@unreal4u.com>
2017-12-09 20:33:00 +0100

    Format DummyLogger according to PSR

--- src/DummyLogger.php
+++ src/DummyLogger.php
@@ -12,55 +12,47 @@ use Psr\Log\LogLevel;
  */
 final class DummyLogger implements LoggerInterface
 {
-    public function emergency($message, array $context = array()): bool
+    public function emergency($message, array $context = array())
     {
-        return $this->log(LogLevel::EMERGENCY, $message, $context);
+        $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert($message, array $context = array()): bool
+    public function alert($message, array $context = array())
     {
-        return $this->log(LogLevel::ALERT, $message, $context);
+        $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = array()): bool
+    public function critical($message, array $context = array())
     {
-        return $this->log(LogLevel::CRITICAL, $message, $context);
+        $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error($message, array $context = array()): bool
+    public function error($message, array $context = array())
     {
-        return $this->log(LogLevel::ERROR, $message, $context);
+        $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning($message, array $context = array()): bool
+    public function warning($message, array $context = array())
     {
-        return $this->log(LogLevel::WARNING, $message, $context);
+        $this->log(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice($message, array $context = array()): bool
+    public function notice($message, array $context = array())
     {
-        return $this->log(LogLevel::NOTICE, $message, $context);
+        $this->log(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info($message, array $context = array()): bool
+    public function info($message, array $context = array())
     {
-        return $this->log(LogLevel::INFO, $message, $context);
+        $this->log(LogLevel::INFO, $message, $context);
     }
 
-    public function debug($message, array $context = array()): bool
+    public function debug($message, array $context = array())
     {
-        return $this->log(LogLevel::DEBUG, $message, $context);
+        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
-    /**
-     * @SuppressWarnings("unused")
-     * @param mixed $level
-     * @param string $message
-     * @param array $context
-     * @return bool
-     */
-    public function log($level, $message, array $context = array()): bool
+    public function log($level, $message, array $context = array())
     {
-        return false;
     }
 }
