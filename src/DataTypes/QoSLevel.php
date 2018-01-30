800837094b3662593b929967eedd9652afc55999
Camilo Sperberg <me@unreal4u.com>
2018-01-30 01:28:07 +0100

    Introducing VO's

---
+++ src/DataTypes/QoSLevel.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unreal4u\MQTT\DataTypes;
+
+use unreal4u\MQTT\Exceptions\InvalidQoSLevel;
+
+/**
+ * This Value Object will always contain a valid QoS level.
+ */
+final class QoSLevel
+{
+    /**
+     * This field indicates the level of assurance for delivery of an Application Message. Can be 0, 1 or 2
+     *
+     * 0: At most once delivery (default)
+     * 1: At least once delivery
+     * 2: Exactly once delivery
+     *
+     * @var int
+     */
+    private $qosLevel = 0;
+
+    /**
+     * QoSLevel constructor.
+     * @param int $QoSLevel
+     * @throws \unreal4u\MQTT\Exceptions\InvalidQoSLevel
+     */
+    public function __construct(int $QoSLevel = 0)
+    {
+        if ($QoSLevel > 2 || $QoSLevel < 0) {
+            throw new InvalidQoSLevel(sprintf(
+                'The provided QoS level is invalid. Valid values are 0, 1 and 2 (Provided: %d)',
+                $QoSLevel
+            ));
+        }
+    }
+
+    /**
+     * Gets the current QoS level
+     *
+     * @return int
+     */
+    public function getQoSLevel(): int
+    {
+        return $this->qosLevel;
+    }
+}
---
+++ tests/ActualTests/DataTypes/QosLevelTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\unreal4u\MQTT\Application;
+
+use PHPUnit\Framework\TestCase;
+use unreal4u\MQTT\DataTypes\QoSLevel;
+use unreal4u\MQTT\Exceptions\InvalidQoSLevel;
+
+class QosLevelTest extends TestCase
+{
+    public function test_invalidQoSLevel()
+    {
+        $this->expectException(InvalidQoSLevel::class);
+        new QoSLevel(-1);
+    }
+
+    public function provider_validQoSLevels(): array
+    {
+        $mapValues[] = [0];
+        $mapValues[] = [1];
+        $mapValues[] = [2];
+
+        return $mapValues;
+    }
+
+    /**
+     * @dataProvider provider_validQoSLevels
+     * @param int $level
+     */
+    public function test_validQoSLevels(int $level)
+    {
+        $QoSLevel = new QoSLevel($level);
+        $this->assertSame($level, $QoSLevel->getQoSLevel());
+    }
+}
