f3eba8a64e5678486caf466bd281c31bf6d6025f
Camilo Sperberg <c.sperberg@bedandbreakfast.eu>
2018-03-16 17:33:14 +0100

    Preparing the possible warning reporting for this VO

--- src/DataTypes/ClientId.php
+++ src/DataTypes/ClientId.php
@@ -24,30 +24,6 @@ final class ClientId
      */
     public function __construct(string $clientId)
     {
-        if ($clientId !== '') {
-            $this->clientId = $clientId;
-            $clientIdSize = \strlen($this->clientId);
-            $utf8ClientIdSize = \mb_strlen($this->clientId);
-
-            if ($clientIdSize !== $utf8ClientIdSize) {
-                $this->logger->warning('The broker MAY reject the connection because of invalid characters');
-            }
-
-            if ($utf8ClientIdSize > 23) {
-                $this->logger->warning('The broker MAY reject the connection because the ClientId is too long');
-            }
-        } else {
-            /*
-             * If you ever wind up in this situation, search for MQTT-3.1.3-7 on the following document for more
-             * information: http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718067
-             */
-            $this->logger->warning('ClientId size is 0 bytes. This has several implications, check comments', [
-                'file' => __FILE__,
-                'line' => __LINE__,
-            ]);
-            $this->cleanSession = true;
-        }
-
         $this->clientId = $clientId;
     }
 
@@ -64,5 +40,31 @@ final class ClientId
     public function __toString(): string
     {
         return $this->getClientId();
+    }
+
+    public function isEmptyClientId(): bool
+    {
+        return $this->clientId === '';
+    }
+
+    public function performStrictValidationCheck(): string
+    {
+        $possibleProblem = '';
+
+        $utf8ClientIdSize = \mb_strlen($this->clientId);
+
+        if ($this->isEmptyClientId()) {
+            /*
+             * If you ever wind up in this situation, search for MQTT-3.1.3-7 on the following document for more
+             * information: http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718067
+             */
+            $possibleProblem = 'ClientId size is 0 bytes. This has several implications, check comments';
+        } elseif ($utf8ClientIdSize > 23) {
+            $possibleProblem = 'The broker MAY reject the connection because the ClientId is too long';
+        } elseif (\strlen($this->clientId) !== $utf8ClientIdSize) {
+            $possibleProblem = 'The broker MAY reject the connection because of invalid characters';
+        }
+
+        return $possibleProblem;
     }
 }
